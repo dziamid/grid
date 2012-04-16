c8a46c3b71ab10823f42e098fe51e3354c9f1a8a
Dziamid <dziamid@gmail.com>
2012-04-16 12:44:07 +0300

    added sample stylesheets and App.Config

--- src/Belectrika/GridBundle/Controller/DefaultController.php
+++ src/Belectrika/GridBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Belectrika\GridBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -14,6 +15,18 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return array('name' => 'John');
+        return array('url' => $this->get('router')->generate('itemsList'));
+    }
+
+    /**
+     * @Route("/items", name="itemsList")
+     */
+    public function itemsActions()
+    {
+        $items = array();
+        for ($i=1; $i<100; $i++) {
+            $items[] = array('title' => "Item {$i}", 'price' => $i*4560, 'amount' => $i*123);
+        }
+        return new Response(json_encode($items));
     }
 }
--- src/Belectrika/GridBundle/Resources/public/js/grid.js
+++ src/Belectrika/GridBundle/Resources/public/js/grid.js
@@ -1,1 +1,28 @@
-console.log('this worked');
+var PriceItem = function (data) {
+    var self = this;
+    self.title = data.title;
+    self.price = ko.observable(data.price);
+    self.amount = ko.observable(data.amount);
+
+};
+
+function MenuViewModel(config) {
+    var self = this;
+
+    self.PriceItems = ko.observableArray([
+        new PriceItem({title: 'Item title', price: 10500, amount: 10})
+    ]);
+
+    self.preload = function () {
+        $.getJSON(config.url, function (data) {
+            for (var i=0; i<data.length; i++) {
+                var item = new PriceItem(data[i]);
+                self.PriceItems.push(item);
+            }
+        });
+    };
+
+    self.preload();
+}
+
+ko.applyBindings(new MenuViewModel(App.Config));
