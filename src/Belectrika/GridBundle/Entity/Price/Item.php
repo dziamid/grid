50318c3252c07859c19cf729d939470667be0c7a
Dziamid <dziamid@gmail.com>
2012-04-17 12:15:53 +0300

    added Price/Item entity

---
+++ src/Belectrika/GridBundle/Entity/Price/Item.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Belectrika\GridBundle\Entity\Price;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Belectrika\GridBundle\Entity\Price\Item
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Belectrika\GridBundle\Entity\Price\ItemRepository")
+ */
+class Item
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $title
+     *
+     * @ORM\Column(name="title", type="string", length=1024)
+     */
+    private $title;
+
+    /**
+     * @var float $price
+     *
+     * @ORM\Column(name="price", type="float")
+     */
+    private $price;
+
+    /**
+     * @var integer $amount
+     *
+     * @ORM\Column(name="amount", type="integer")
+     */
+    private $amount;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set price
+     *
+     * @param float $price
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * Get price
+     *
+     * @return float 
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set amount
+     *
+     * @param integer $amount
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+    }
+
+    /**
+     * Get amount
+     *
+     * @return integer 
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+}
